037884316154c23f594ed81ee3d5b7ad09f5db80
Steven Fawcett <steven.fawcett@me.com>
2025-08-08 15:55:45 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using PluginBase;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace MSFSPlugin
+{
+    public class Plugin : BroadcastPlugin
+    {
+        public override string Stanza => "MSFS";
+        public override void Start()
+        {
+            Debug.WriteLine($"Starting {Name} plugin.");
+            // This Plugin does not do anything on start, but it could be used to initialize resources or connections.
+        }
+
+        public Plugin() : base()
+        {
+            Name = "MFSF Plugin";
+            Description = "Microsoft Flight Simulator Plugin.";
+            Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
+            Icon = Properties.Resources.red;
+        }
+    }
+}
