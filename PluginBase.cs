49fcf519f56314281934ecbed3f171480eab90b9
Steven Fawcett <steven.fawcett@me.com>
2025-08-15 15:49:31 +0100

    API plugin now calling master cache variable

--- PluginBase.cs
+++ PluginBase.cs
@@ -4,19 +4,19 @@ using System.Reflection;
 
 namespace MSFSPlugin
 {
-    public class Plugin : BroadcastPlugin
+    public class PluginBase : BroadcastPlugin
     {
         public override string Stanza => "MSFS";
         public override void Start()
         {
             Debug.WriteLine($"Starting {Name} plugin.");
-            // This Plugin does not do anything on start, but it could be used to initialize resources or connections.
+            // This PluginBase does not do anything on start, but it could be used to initialize resources or connections.
         }
 
-        public Plugin() : base()
+        public PluginBase() : base()
         {
-            Name = "MFSF Plugin";
-            Description = "Microsoft Flight Simulator Plugin.";
+            Name = "MFSF PluginBase";
+            Description = "Microsoft Flight Simulator PluginBase.";
             Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
             Icon = Properties.Resources.red;
         }
