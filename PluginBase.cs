85869faeebcd48d5df458a3e532b00598492373f
Steven Fawcett <steven.fawcett@me.com>
2025-08-18 16:22:21 +0100

    Improve handling of services.

--- PluginBase.cs
+++ PluginBase.cs
@@ -1,23 +1,18 @@
 ﻿using BroadcastPluginSDK;
-using System.Diagnostics;
-using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
 
 namespace MSFSPlugin
 {
     public class PluginBase : BroadcastPluginBase
     {
-        public override string Stanza => "MSFS";
-        public override string Start()
+        public PluginBase( IConfiguration configuration) : base(
+            configuration , null,
+            Properties.Resources.red,
+            "FlightSim",
+            "MSFS",
+            "Microsoft Flight Simulator PluginBase.")
         {
-            return "This PluginBase does not do anything on start, but it could be used to initialize resources or connections.";
-        }
-
-        public PluginBase() : base()
-        {
-            Name = "MFSF PluginBase";
-            Description = "Microsoft Flight Simulator PluginBase.";
-            Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
-            Icon = Properties.Resources.red;
         }
     }
 }
